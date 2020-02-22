98107e10537509d47cb6724910ca8e4c5eeef692
yumefx <yumevfx@gmail.com>
2020-02-22 23:15:38 +0800

    version 0.1

---
+++ RDW_client.py
@@ -0,0 +1,150 @@
+import os
+import sys
+import time
+import tkinter
+import tkinter.messagebox
+import win32api,win32con,pywintypes
+from datetime import datetime
+from getpass import getuser
+from socket import *
+from threading import Thread
+
+def checkRemote(user):
+    usersLog = os.popen("quser")
+    userlist = usersLog.read().split("\n")
+    del userlist[0]
+    del userlist[-1]
+    for ul in userlist:
+        if (">" + user) == ul[:len(user) + 1]:
+            userInfo = ul.split(" ")
+            userInfo = [i for i in userInfo if i != ""]
+            if "rdp-tcp" in userInfo[1]:
+                return True
+    return False
+
+def getQueueText(text):
+    space, str1, str2 = 70,"",""
+    for i in range(5):
+        str1 += text + " "* space
+    str2 = " " * space +str1 + "\n\n\n\n"
+    str1 += "\n\n\n\n"
+    return (str1 + str2) * 10
+
+class watermark(tkinter.Tk):
+    procHide = False
+    host = gethostname()
+    ip = "192.168.1.15"
+    port = "54321"
+    conn = None
+    company = "yumefx.com"
+
+    def __init__(self,*args,**kwargs):
+        super().__init__()
+        self.text = tkinter.StringVar()
+        self.text.set(getQueueText(self.company))
+
+        #connect to server
+        connThread = Thread(target = self.linkServer,args=(self.ip,self.port))
+        connThread.start()
+
+        #sent message that this service online
+        self.trySendMsg(getuser(),datetime.now(),"add")
+        self.run()
+
+    def trySendMsg(self,name,date,state):
+        sendThread = Thread(target=self.sendMsg,args=(name,date,state))
+        sendThread.start()
+
+    def senfMsg(self,name,date,state):
+        #if send failed,try 3 times
+        for i in range(3):
+            try:
+                self.conn.send(self.host + "\n" + name + "\n" + str(date) + "\n" + state)
+                return True
+            except:
+                pass
+            time.sleep(0.5)
+
+        #when all 3 times failed,print log to localpath.
+        logFileDir = "C:/watermark/" + self.host
+        logFilePath = logFileDir + "/" + name + "log"
+        logFile = None
+        if not os.path.isdir(logFileDir):
+            os.makedirs(logFileDir)
+        if not os.path.isfile(logFilePath):
+            logFile = open(logFilePath,"w")
+        else:
+            logFile = open(logFilePath,"a")
+        logFile.write(str(date) + ":" + state)
+        logFile.close()
+
+    def linkServer(self,IP,Port):
+        buffsize = 1024
+        self.conn = socket(AF_INET,SOCK_STREAM)
+        try:
+            self.comm.connect((IP,Port))
+        except:
+            #this can show error window,but this service need be quiet for user,so comment it.
+            #and if link failed,save log to local path(in def sendMsg).
+            #tkinter.messagebox.showerror("Error","Can not link to the server,check that service.")
+            self.conn = 2
+
+    def refresh(self):
+        currentUser = getuser()
+
+        #hide watermark when no remote user
+        if not checkRemote(currentUser) and (not self.procHide):
+            self.withdraw()
+            self.procHide = True
+            self.trySendMsg(currentUser,datetime.now(),"logout")
+        #show watermark when user remote link
+        elif checkRemote(currentUser) and self.procHide:
+            #change watermark content ,add the user name
+            self.text.set(getQueueText(self.company + "-" + currentUser))
+            self.update()
+            self.deiconify()
+            self.procHide = False
+            self.trySendMsg(currentUser,datetime.now(),"login")
+        #refresh every 0.5s
+        self.after(500,self.refresh())
+
+    def run(self):
+        #actually this can put in def refresh,to get the remote desktop size
+        #and i just set to 4000
+        #what? you have a 8k screen,nevermind
+        width = 4000 #win32api.GetSystemMetrics(0)
+        hight = 4000 #win32api.GetSystemMetrics(1)
+        self.overrideredirect(True)                      #hide the window bounding box
+        self.lift()                                      #set window on top
+        self.attributes("-alpha",0.6)                    #make watermark transparent
+        self.WM_attributes("-topmost",True)              #always set window on top
+        self.WM_attributes("-disabled",True)             #disable window,so mouse can not click
+        self.WM_attributes("-tansparentcolor","white")   #window background set white and transparent
+        hwindow = pywintypes.HANDLE(int(self.frame),16)
+        exStyle = win32con.WS_EX_COMPOSITED | win32con.WS_EX_LAYERED | win32con.WS_EX_NOACTIVATE | win32con.WS_EX_TOPMOST | win32con.WS_EX_TRANSPARENT
+        win32api.SetWindowLong(hwindow,win32con.GWL_EXSTYLE,exStyle)
+        #set text / font / text color / background color
+        label = tkinter.Label(textvarieble=self.text,font=("Times New Roman","40"),fg="#d5d5d5",bg="white")
+        label.pack()
+        self.withdraw()
+        self.procHide = True
+        self.refresh()
+        self.mainloop()
+
+if __name__ == "__main__":
+    WM = watermark()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ RDW_server.py
@@ -0,0 +1,129 @@
+import sys
+import os
+import re
+import datetime
+import time
+
+from socket import *
+from threading import Thread
+
+from email.header import Header
+from email.mime.text import MIMEText
+from email.utils import parseaddr,formataddr
+import smtplib
+
+address = "192.168.1.15"
+port = 54321
+buffsize = 1024
+Msock = socket(AF_INET,SOCK_STREAM)
+Msock.bind((address,port))
+Msock.listen(24)        #set max connect 24,it is enough for me
+conn_list = []         #save connecting connect
+conn_hist = []         #save last log for every connect
+conn_dt = {}
+logStorage = "C:/watermarkServer/"
+acccount = "yumefx@yumefx.com"
+password = "123456"   #some mail put the offer code in mail client settings , not the password, just like QQmail.
+toMailAddrs = ["admin@yumefx.com","develope@yumefx.com"]
+smtpServer = "smtp.yumefx.com"
+
+
+def formatAddress(s):
+    name,addr = parseaddr(s)
+    return formataddr((Header(name,"utf-8").encode(),addr))
+
+def sendmail(title,text):
+    server = smtplib.SMTP(smtpServer,25)    #25 is default smtp port, check mail setting
+    server.set_debuglevel(1)
+    server.login(acccount,password)
+
+    msg = MIMEText(text,"plain","utf-8")
+    msg["Form"] = formatAddress("Watermark Service<%s>" %acccount)
+    msg["Subject"] = Header(title,"utf-8").encode()
+
+    for ta in toMailAddrs:
+        msg["To"] = formatAddress("SiteGroup<%s>" % ta)
+        server.sendmail(acccount,ta,msg.as_string())
+    server.quit()
+
+def tcplink(sock,addr):
+    while True:
+        try:
+            #accept message and save
+            recvData = sock.recv(buffsize).decode("utf-8")
+            print(recvData,addr)
+            conn_hist[conn_list.index(addr)] = recvData
+            recvDatas = recvData.split("\n")
+            #host + "\n" + name + "\n" + date + "\n" + state
+            printLog(recvDatas[0],addr[0],recvDatas[1],recvDatas[2],recvDatas[3])
+            if recvDatas[3] == "login":
+                sendmail("watermark service user login:" + recvDatas[1],
+                         "user login succeed,now show watermark.\nuse machine:" + recvDatas[0] + "\nuser account:" +
+                         recvDatas[1] + "\nIP:" + addr[0] + "\nlogin time:" + recvDatas[2])
+            else:
+                sendmail("watermark service user logout:" + recvDatas[1],
+                         "user logout succeed,now hide watermark.\nuse machine:" + recvDatas[0] + "\nuser account:" +
+                         recvDatas[1] + "\nIP:" + addr[0] + "\nlogout time:" + recvDatas[2])
+            if not recvData:
+                break
+
+        except:
+            #when break connect send warning mail
+            sock.close()
+            print(addr,"offline")
+            lastLog = conn_hist[conn_list.index(addr)]
+            print(lastLog)
+            sendmail("watermark offline warning!","IP:" + addr[0] + "\nthe last log is:\n" + lastLog)
+            conn_dt.pop(addr)
+            conn_list.pop(addr)
+            conn_hist.pop(lastLog)
+            break
+
+def printLog(host,ip,user,date,state):
+    hostDir = logStorage + host + "-" + ip
+    if not os.path.isdir(hostDir):
+        os.makedirs(hostDir)
+    logFilePath = hostDir + "/" + user + ".log"
+    logfile = None
+    if not os.path.isfile(logFilePath):
+        logfile = open(logFilePath,"w")
+    else:
+        logfile = open(logFilePath,"a")
+    logfile.write(date + ":" + state)
+    logfile.close()
+
+def recs():
+    while True:
+        clientsock,clientaddress = Msock.accept()
+        if clientaddress not in conn_list:
+            conn_list.append(clientaddress)
+            conn_hist.append("")
+            conn_dt[clientaddress] = clientsock
+        print("connect from:",clientaddress)
+
+        #start thread
+        t = Thread(target=tcplink,args=(clientsock,clientaddress))
+        t.start()
+        tj = Thread(target=sendLog)
+        tj.start()
+
+def sendLog():
+    while True:
+        #send current watermark state every 4 hours
+        time.sleep(14400)
+        mailText = "Now there are " + str(len(conn_list)) + "client is online\nthese last log is:"
+        for i in range(len(conn_list)):
+            mailText += conn_list[i][0] + ": " + conn_hist[i].replace("\n","  ") + "\n"
+        sendmail("watermark service current state")
+
+if __name__  == "__main__":
+    recs()
+
+
+
+
+
+
+
+
+
